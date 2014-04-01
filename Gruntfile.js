1735bab9f614fc0d562a43cf0b02f5b9fd8f54d4
Eran <me@eran.sh>
2014-04-01 16:17:04 -0400

    First commit

---
+++ Gruntfile.js
@@ -0,0 +1,42 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    meta: {
+      themePath: grunt.option('theme')
+    },
+
+    sass: {
+      dist: {
+        files: {
+          '<%= meta.themePath %>style.css': '<%= meta.themePath %>style.scss'
+        }
+      }
+    },
+
+    autoprefixer: {
+      single_file: {
+        src: '<%= meta.themePath %>style.css'
+      }
+    },
+
+    watch: {
+      scripts: {
+        files: ['<%= meta.themePath %>style.scss'],
+        tasks: ['sasspress']
+      }
+    }
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+  grunt.registerTask('default', ['watch']);
+  grunt.registerTask('sasspress', ['sass', 'autoprefixer']);
+
+};
+
